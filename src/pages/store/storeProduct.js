c5f08f6229d2dccb360b23272caa75568bd0ed57
activenoob <yusufkehinde11@gmail.com>
2020-11-24 17:11:14 +0100

    added the ads part

--- src/pages/store/storeProduct.js
+++ src/pages/store/storeProduct.js
@@ -1,9 +1,20 @@
 import React from "react";
+import { ReactComponent as Grey } from "../../svg/shirt.svg";
 
 const StoreProduct = () => {
   return (
-    <div>
-      <h1>bag and price</h1>
+    <div className="storeproduct">
+      <div className="productads">
+        <div className="productads-img">
+          <Grey />
+        </div>
+        <div className="productads-info">
+          <h1>Shop with ease</h1>
+          <h3>
+            Checkout our daily deals <span>Click hear</span>{" "}
+          </h3>
+        </div>
+      </div>
     </div>
   );
 };
